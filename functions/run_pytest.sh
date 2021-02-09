693c5320a336c8bbe8cac168b7abf02d12158959
kuratakenichi <kuratakenichi@nifty.com>
2021-02-09 23:04:58 +0900

    fix2

--- functions/run_pytest.sh
+++ functions/run_pytest.sh
@@ -4,8 +4,5 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 pip install \
- -r file_validation/requirements.txt \
- -r master_period_validation/requirements.txt \
- -r validation_ok_handler/requirements.txt \
- -r validation_ng_handler/requirements.txt
+ -r requirements.txt
 pytest -p no:cacheprovider . -v
